770bea4da2c8a49411d6055e853572ab24f1407a
Yiyi Wang <shd101wyy@sina.com>
2016-04-27 09:39:17 -0500

    preload images

--- www/bundle.js
+++ www/bundle.js
@@ -60,6 +60,18 @@
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+	function preload() {
+	  var images = new Array();
+	  for (var i = 0; i < arguments.length; i++) {
+	    images[i] = new Image();
+	    images[i].src = arguments[i];
+	  }
+	  return images;
+	}
+
+	// preload images for faster rendering
+	preload('./images/jie.png', './images/suicide.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/w1.png', './images/b.png', './images/mark_b.png', './images/mark_w.png', './images/default-user-64.png');
+
 	// loading screen
 	$('.loading-screen .logo').fadeIn(1000, function () {
 	  setTimeout(function () {
